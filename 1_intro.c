2bc67224cc334520680f4dbd03342047e8f0cca4
TRax404 <tiryho22@gmail.com>
2026-08-27 23:37:14 +0600

    add new

---
+++ 1_intro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+}
+
+// int main() {
+//     int a = 10;
+//     int b  =10;
+//     int sum   = a+ b;
+//     printf("The sum of  and  is %d\n", sum);
+// }
+
+// int main() {
+//     int a = 10;
+//     int b = 10;
+//     int c = 30 ;
+//     int sum =( a + b )- c;
+//     printf("The calculate number  is  %d\n",  sum);
+//     return 0;
+// }
+
---
+++ 2_variable_and_datatype.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+// c has many data types like int, float, double, char, etc.
+
+//  ------- Example  --------- 
+// 1: int  = 10; // integer data type
+// 2: float = 10.5; // float data type
+// 3: double = 10.5; // double data type
+// 4: char = 'a'; // character data type
+
+
+// int main() {
+//     int a = 10; // integer data type
+//     float b = 10.5; // float data type
+//     double c = 10.5; // double data type
+//     char d = 'a'; // character data type
+//     printf("The value of a is %d\n", a);
+//     printf("The value of b is %f\n", b);
+//     printf("The value of c is %lf\n", c);
+//     printf("The value of d is %c\n", d);
+//     // sizeof(a);
+//     printf("The size of a is %zu bytes\n", sizeof(a));
+
+//     return 0;
+// }
+
+
+// int , float , double, char 
+
+// int main () {
+//     int a , b ;
+//     printf("Enter the value of a and b : ");
+
+//     scanf("%d %d", &a, &b);
+
+//     printf("The value of a is %d and b is %d\n", a, b);
+//     return 0;   
+// }
+
+// area of square 
+
+int main () {
+    int side ;
+    printf("Enter the side of square : ");
+    scanf("%d",&side);
+    int area =3.14 * side * side ;
+
+    printf(" The area square is %d\n", area);
+    return 0;
+}
+
+
+
+
