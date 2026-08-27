4a65e41298c5b2f912ef3dce17e2e48d93869059
TRax404 <tiryho22@gmail.com>
2026-08-27 23:56:56 +0600

    relation operator

---
+++ 3_operator.c
@@ -0,0 +1,34 @@
+// start all operators in c
+
+
+// relation operators example
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+    int b = 5;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    // Greater than
+    printf("a > b  : %d\n", a > b);
+
+    // Less than
+    printf("a < b  : %d\n", a < b);
+
+    // Greater than or equal
+    printf("a >= b : %d\n", a >= b);
+
+    // Less than or equal
+    printf("a <= b : %d\n", a <= b);
+
+    // Equal to
+    printf("a == b : %d\n", a == b);
+
+    // Not equal to
+    printf("a != b : %d\n", a != b);
+
+    return 0;
+}
