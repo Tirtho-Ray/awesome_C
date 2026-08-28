5b977d77f522c367cd2f65b9bb06243dc56bb433
TRax404 <tiryho22@gmail.com>
2026-08-28 23:01:03 +0600

    try condition

--- 3_operator.c
+++ 3_operator.c
@@ -1,54 +1,71 @@
-// start all operators in c
+// // start all operators in c
 
 
-// relation operators example
+// // relation operators example
+// // #include <stdio.h>
+
+// // int main() {
+
+// //     int a = 10;
+// //     int b = 5;
+
+// //     printf("a = %d\n", a);
+// //     printf("b = %d\n\n", b);
+
+// //     // Greater than
+// //     printf("a > b  : %d\n", a > b);
+
+// //     // Less than
+// //     printf("a < b  : %d\n", a < b);
+
+// //     // Greater than or equal
+// //     printf("a >= b : %d\n", a >= b);
+
+// //     // Less than or equal
+// //     printf("a <= b : %d\n", a <= b);
+
+// //     // Equal to
+// //     printf("a == b : %d\n", a == b);
+
+// //     // Not equal to
+// //     printf("a != b : %d\n", a != b);
+
+// //     return 0;
+// // }
+
+
 // #include <stdio.h>
 
 // int main() {
 
-//     int a = 10;
-//     int b = 5;
+//     int age = 20;
+//     int marks = 75;
 
-//     printf("a = %d\n", a);
-//     printf("b = %d\n\n", b);
+//     // Logical AND (&&)
+//     printf("AND: %d\n", age >= 18 && marks >= 40);
 
-//     // Greater than
-//     printf("a > b  : %d\n", a > b);
+//     // Logical OR (||)
+//     printf("OR: %d\n", age >= 18 || marks >= 90);
 
-//     // Less than
-//     printf("a < b  : %d\n", a < b);
-
-//     // Greater than or equal
-//     printf("a >= b : %d\n", a >= b);
-
-//     // Less than or equal
-//     printf("a <= b : %d\n", a <= b);
-
-//     // Equal to
-//     printf("a == b : %d\n", a == b);
-
-//     // Not equal to
-//     printf("a != b : %d\n", a != b);
+//     // Logical NOT (!)
+//     printf("NOT: %d\n", !(age >= 18));
 
 //     return 0;
 // }
 
-
 #include <stdio.h>
 
 int main() {
+    int age;
 
-    int age = 20;
-    int marks = 75;
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-    // Logical AND (&&)
-    printf("AND: %d\n", age >= 18 && marks >= 40);
-
-    // Logical OR (||)
-    printf("OR: %d\n", age >= 18 || marks >= 90);
-
-    // Logical NOT (!)
-    printf("NOT: %d\n", !(age >= 18));
+    if (age >= 18) {
+        printf("You are an adult\n");
+    } else {
+        printf("You are a minor\n");
+    }
 
     return 0;
 }
